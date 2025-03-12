b8a39e89505c4666f0ba1dedbd882605f40d8a51
SharkSharko <melileiemmanuel@gmail.com>
2025-03-12 18:08:29 +0300

    Complete variables lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+
+// Declare companyName as a constant and set it to "Scuber"
+const companyName = "Scuber";
+
+// Declare mostProfitableNeighborhood using let and assign a value
+let mostProfitableNeighborhood = "Chelsea";
+
+// Declare companyCeo using let and assign a value
+let companyCeo = "Susan Smith";
+// Code your solution in this file!
